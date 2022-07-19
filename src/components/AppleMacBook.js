2f5a33bcc5e72c4b6fd1379d06fb3b83c8e27742
woosang <woosang3078@hanmail.net>
2022-07-19 16:13:32 +0900

    AppleMacBook Buy Page

---
+++ src/components/AppleMacBook.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Section from './SectionBox';
+import Header from './Header';
+import DetailHeader from './DetailHeader';
+import CompareContainer from './CompareBox';
+
+function AppleMacBook () {
+    return (
+        <>
+         <Header />
+         <DetailHeader />
+         <Section />
+         <CompareContainer />
+        </>
+    )
+};
+
+export default AppleMacBook;
